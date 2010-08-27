d58472103732a216e2604f4dbab8a3206477eb58
fmassei <fmassei@9695257f-d6d3-1698-6185-2ce6fa29c978>
2010-08-27 13:29:25 +0000

    - barray_search function changed - more doxy comments

--- trunk/mmp_barray.h
+++ trunk/mmp_barray.h
@@ -32,34 +32,48 @@
 #include "mmp_mmap.h"
 #include "mmp_files.h"
 
+/** \brief barray index datatype */
 typedef unsigned int t_mmp_barray_idx;
 
+/** \brief internal barray record (DO NOT USE!) */
 typedef struct mmp_barray_rec_s {
-    int present:1;
-    unsigned int idx:(sizeof(t_mmp_barray_idx)*8-1);
+    int present:1;                                  /**< record present? */
+    unsigned int idx:(sizeof(t_mmp_barray_idx)*8-1);/**< record index */
     /* data follows */
 } t_mmp_barray_rec_s;
 
+/** \brief internal barray page (DO NOT USE!) */
 typedef struct mmp_barray_page_s {
-    t_mmp_mmap_s *records;
-    int dirty:1;
-    unsigned int n_data:(sizeof(uint16_t)*8-1);
-    t_mmp_barray_idx start, end;
+    t_mmp_mmap_s *records;                          /**< ptr to records */
+    int dirty:1;                                    /**< page dirty? */
+    unsigned int n_data:(sizeof(uint16_t)*8-1);     /**< number of records */
+    t_mmp_barray_idx start;                         /**< first index */
+    t_mmp_barray_idx end;                           /**< last index */
 } t_mmp_barray_page_s;
 
+/** \brief barray struct */
 typedef struct mmp_barray_s {
-    int fd;
-    t_mmp_barray_page_s **pages;
-    unsigned int n_pages, page_len, data_size, rec_size, recs_per_page;
+    int fd;                                         /**< underlaying file */
+    t_mmp_barray_page_s **pages;                    /**< pages */
+    unsigned int n_pages;                           /**< number of pages */
+    unsigned int page_len;                          /**< page length */
+    unsigned int data_size;                         /**< record data size */
+    unsigned int rec_size;                          /**< record size */
+    unsigned int recs_per_page;                     /**< records per page */
 } t_mmp_barray_s;
 
+/** \brief create a barray */
 t_mmp_barray_s *mmp_barray_create(const char *fname, unsigned int page_size,
                                     unsigned int data_size);
+/** \brief insert data into a barray */
 ret_t mmp_barray_insert(t_mmp_barray_s * __restrict barray,
                         t_mmp_barray_idx idx, const void * __restrict data);
+/** \brief search the barray for a given index */
 ret_t mmp_barray_search(t_mmp_barray_s * __restrict barray,
-                        t_mmp_barray_idx idx, void * __restrict data);
+                        t_mmp_barray_idx idx, void ** __restrict data);
+/** \brief delete the barray data at a given index */
 ret_t mmp_barray_delete(t_mmp_barray_s *barray, t_mmp_barray_idx idx);
+/** \brief destroy the barray */
 void mmp_barray_destroy(t_mmp_barray_s **barray);
 
 #ifdef UNIT_TESTING
