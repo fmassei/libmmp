2cab29d56f7cd5a058b7889b458005039d8ae1a6
fmassei <fmassei@9695257f-d6d3-1698-6185-2ce6fa29c978>
2010-10-27 16:44:01 +0000

    - moved to LGPL license

--- mmp_date.h
+++ mmp_date.h
@@ -1,22 +1,21 @@
 /*
     Copyright 2010 Francesco Massei
 
-    This file is part of the MMP library.
+    This file is part of the libmmp library.
 
-        MMP is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
+        libmmp is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    MMP is distributed in the hope that it will be useful,
+    libmmp is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with MMP.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU Lesser General Public License
+    along with libmmp.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 /** \file   mmp_date.h
  * \brief   common date management functions
  * \author  FtM
