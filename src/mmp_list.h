a6415bb11d5b5b68f907b5d4a6cc61d48e067afb
fmassei <fmassei@9695257f-d6d3-1698-6185-2ce6fa29c978>
2011-12-21 21:20:30 +0000

    - mmp_list tests moved to checks

--- src/mmp_list.h
+++ src/mmp_list.h
@@ -123,9 +123,4 @@ MMP_API void mmp_list_sort_by_data(t_mmp_list_s * __restrict list,
 
 MMP_CEXTERN_END
 
-#ifdef UNIT_TESTING
-#include "mmp_tap.h"
-ret_t mmp_list_unittest(t_mmp_tap_cycle_s *cycle);
-#endif /* UNIT_TESTING */
-
 #endif /* H_MMP_LIST_H */
